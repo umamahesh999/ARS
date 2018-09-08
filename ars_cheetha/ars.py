5ad401f3c16019e6d4d9ab51cc0f66113a54ae38
uma-clicksco <uma.pinninti@clicksco.com>
2018-09-08 17:50:39 +0530

    Normalizing the states

--- ars_cheetha/ars.py
+++ ars_cheetha/ars.py
@@ -8,7 +8,7 @@ Created on Sat Sep  8 15:21:24 2018
 
 # Importing libraries
 import os
-import numpy
+import numpy as np
 
 # Setting Hyper Params
 
@@ -24,5 +24,30 @@ class HyperParamers():
         self.noise = 0.03
         self.seed = 1
         self.env_name = ""
-        
     
+
+# Normalizing the states
+        
+class Normalizer():
+    
+    def __init__(self, nb_inputs):
+        self.n = np.zeros(nb_inputs)
+        self.mean = np.zeros(nb_inputs)
+        self.mean_diff = np.zeros(nb_inputs)
+        self.var = np.zeros(nb_inputs)
+        
+    def observe(self, x):
+        self.n += 1.
+        last_mean = self.mean.copy()
+        self.mean += (x - self.mean) / self.n
+        self.mean_diff += (x - last_mean) * (x - self.mean)
+        self.var = (self.mean_diff / self.n).clip(min = 1e-2)
+    
+    def normalize(self, inputs):
+        obs_mean = self.mean
+        obs_std = np.sqrt(self.var)
+        return (inputs - obs_mean) / obs_std
+        
+        
+        
+        
